c530db99868626dde92860bfccac111861e31161
Dr E Alskaf <73792019+drskaf@users.noreply.github.com>
2021-06-07 15:52:01 +0100

    Create EHR_transform.py

---
+++ EHR_transform.py
@@ -0,0 +1,279 @@
+## Data preparation
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers
+import tensorflow_probability as tfp
+import matplotlib.pyplot as plt
+import pandas as pd
+import aequitas as ae
+# Put all of the helper functions in utils
+from utils import build_vocab_files, show_group_stats_viz, aggregate_dataset, preprocess_df, df_to_dataset, posterior_mean_field, prior_trainable
+pd.set_option('display.max_columns', 500)
+# this allows you to make changes and save in student_utils.py and the file is reloaded every time you run a code block
+
+#OPEN ISSUE ON MAC OSX for TF model training
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+# Dataset Loading and Schema Review
+
+dataset_path = "./data/final_project_dataset.csv"
+df = pd.read_csv(dataset_path)
+
+# Determine Level of Dataset
+# Line Test
+try:
+    assert len(df) > df['encounter_id'].nunique() 
+    print("Dataset could be at the line level")
+except:
+    print("Dataset is not at the line level")
+    
+# Encounter Test
+try:
+    assert len(df) == df['encounter_id'].nunique()
+    print("Dataset could be at the encounter level")
+except:
+    print("Dataset is not at the encounter level")
+    
+# Analyse dataset
+import tensorflow_data_validation as tfdv
+######NOTE: The visualization will only display in Chrome browser. ########
+full_data_stats = tfdv.generate_statistics_from_csv(data_location='./data/final_project_dataset.csv') 
+tfdv.visualize_statistics(full_data_stats)
+
+# Reduce Dimensionality of the NDC Code Feature
+#NDC code lookup file
+ndc_code_path = "./medication_lookup_tables/final_ndc_lookup_table"
+ndc_code_df = pd.read_csv(ndc_code_path) 
+
+from student_utils import reduce_dimension_ndc
+reduce_dim_df = reduce_dimension_ndc(df, ndc_code_df)
+# Number of unique values should be less for the new output field
+assert df['ndc_code'].nunique() > reduce_dim_df['generic_drug_name'].nunique()
+
+# Select First Encounter for each Patient
+from student_utils import select_first_encounter
+first_encounter_df = select_first_encounter(reduce_dim_df)
+
+# unique patients in transformed dataset
+unique_patients = first_encounter_df['patient_nbr'].nunique()
+print("Number of unique patients:{}".format(unique_patients))
+
+# unique encounters in transformed dataset
+unique_encounters = first_encounter_df['encounter_id'].nunique()
+print("Number of unique encounters:{}".format(unique_encounters))
+
+original_unique_patient_number = reduce_dim_df['patient_nbr'].nunique()
+# number of unique patients should be equal to the number of unique encounters and patients in the final dataset
+assert original_unique_patient_number == unique_patients
+assert original_unique_patient_number == unique_encounters
+print("Tests passed!!")
+
+# Aggregate Dataset to Right Level for Modeling
+exclusion_list = ['generic_drug_name']
+grouping_field_list = [c for c in first_encounter_df.columns if c not in exclusion_list]
+agg_drug_df, ndc_col_list = aggregate_dataset(first_encounter_df, grouping_field_list, 'generic_drug_name')
+assert len(agg_drug_df) == agg_drug_df['patient_nbr'].nunique() == agg_drug_df['encounter_id'].nunique()
+
+# Prepare Fields and Cast Dataset
+agg_drug_df.to_csv('./data/agg_drug.csv')
+agg_drug_data_stats = tfdv.generate_statistics_from_csv(data_location='./data/agg_drug.csv') 
+tfdv.visualize_statistics(agg_drug_data_stats)
+
+'''
+Please update the list to include the features you think are appropriate for the model 
+and the field that we will be using to train the model. There are three required demographic features for the model 
+and I have inserted a list with them already in the categorical list. 
+These will be required for later steps when analyzing data splits and model biases.
+'''
+required_demo_col_list = ['race', 'gender', 'age']
+student_categorical_col_list = [ 'payer_code','medical_specialty', 'primary_diagnosis_code', 'other_diagnosis_codes' ] + required_demo_col_list + ndc_col_list
+student_numerical_col_list = [ 'admission_type_id', 'discharge_disposition_id', 'admission_source_id', 'num_lab_procedures', 'number_diagnoses', 'num_medications', 'num_procedures' ]
+PREDICTOR_FIELD = 'time_in_hospital'
+
+def select_model_features(df, categorical_col_list, numerical_col_list, PREDICTOR_FIELD, grouping_key='patient_nbr'):
+    selected_col_list = [grouping_key] + [PREDICTOR_FIELD] + categorical_col_list + numerical_col_list   
+    return agg_drug_df[selected_col_list]
+selected_features_df = select_model_features(agg_drug_df, student_categorical_col_list, student_numerical_col_list,
+                                            PREDICTOR_FIELD)
+
+processed_df = preprocess_df(selected_features_df, student_categorical_col_list, 
+        student_numerical_col_list, PREDICTOR_FIELD, categorical_impute_value='nan', numerical_impute_value=0)
+
+# Split Dataset into Train, Validation, and Test Partitions
+from student_utils import patient_dataset_splitter
+d_train, d_val, d_test = patient_dataset_spli
+tter(processed_df, 'patient_nbr')
+assert len(d_train) + len(d_val) + len(d_test) == len(processed_df)
+print("Test passed for number of total rows equal!")
+assert (d_train['patient_nbr'].nunique() + d_val['patient_nbr'].nunique() + d_test['patient_nbr'].nunique()) == agg_drug_df['patient_nbr'].nunique()
+print("Test passed for number of unique patients being equal!")
+
+# Demographic Representation Analysis of Split
+show_group_stats_viz(processed_df, PREDICTOR_FIELD)
+show_group_stats_viz(d_train, PREDICTOR_FIELD)
+show_group_stats_viz(d_test, PREDICTOR_FIELD)
+
+# Demographic Group Analysis
+# Full dataset before splitting
+patient_demo_features = ['race', 'gender', 'age', 'patient_nbr']
+patient_group_analysis_df = processed_df[patient_demo_features].groupby('patient_nbr').head(1).reset_index(drop=True)
+show_group_stats_viz(patient_group_analysis_df, 'gender')
+# Training partition
+show_group_stats_viz(d_train, 'gender')
+# Test partition
+show_group_stats_viz(d_test, 'gender')
+   
+# Convert Dataset Splits to TF Dataset
+# Convert dataset from Pandas dataframes to TF dataset 
+batch_size = 128
+diabetes_train_ds = df_to_dataset(d_train, PREDICTOR_FIELD, batch_size=batch_size)
+diabetes_val_ds = df_to_dataset(d_val, PREDICTOR_FIELD, batch_size=batch_size)
+diabetes_test_ds = df_to_dataset(d_test, PREDICTOR_FIELD, batch_size=batch_size)
+# We use this sample of the dataset to show transformations later
+diabetes_batch = next(iter(diabetes_train_ds))[0]
+def demo(feature_column, example_batch):
+    feature_layer = layers.DenseFeatures(feature_column)
+    print(feature_layer(example_batch))
+    
+## Create Categorical Features with TF Feature Columns
+vocab_file_list = build_vocab_files(d_train, student_categorical_col_list)
+from student_utils import create_tf_categorical_feature_cols
+tf_cat_col_list = create_tf_categorical_feature_cols(student_categorical_col_list)
+test_cat_var1 = tf_cat_col_list[0]
+print("Example categorical field:\n{}".format(test_cat_var1))
+demo(test_cat_var1, diabetes_batch)
+
+## Create Numerical Features with TF Feature Columns
+from student_utils import create_tf_numeric_feature
+def calculate_stats_from_train_data(df, col):
+    mean = df[col].describe()['mean']
+    std = df[col].describe()['std']
+    return mean, std
+
+def create_tf_numerical_feature_cols(numerical_col_list, train_df):
+    tf_numeric_col_list = []
+    for c in numerical_col_list:
+        mean, std = calculate_stats_from_train_data(train_df, c)
+        tf_numeric_feature = create_tf_numeric_feature(c, mean, std)
+        tf_numeric_col_list.append(tf_numeric_feature)
+    return tf_numeric_col_list
+  
+tf_cont_col_list = create_tf_numerical_feature_cols(student_numerical_col_list, d_train)
+test_cont_var1 = tf_cont_col_list[0]
+print("Example continuous field:\n{}\n".format(test_cont_var1))
+demo(test_cont_var1, diabetes_batch)
+
+## Build Deep Learning Regression Model with Sequential API and TF Probability Layers
+claim_feature_columns = tf_cat_col_list + tf_cont_col_list 
+claim_feature_layer = tf.keras.layers.DenseFeatures(claim_feature_columns)
+
+optimizer = tf.keras.optimizers.RMSprop(0.001)
+def build_sequential_model(feature_layer):
+    model = tf.keras.Sequential([
+        feature_layer,
+        tf.keras.layers.Dense(150, activation='relu'),
+        tf.keras.layers.Dense(75, activation='relu'),
+        tfp.layers.DenseVariational(1+1, posterior_mean_field, prior_trainable),
+        tfp.layers.DistributionLambda(
+            lambda t:tfp.distributions.Normal(loc=t[..., :1],
+                                             scale=1e-3 + tf.math.softplus(0.01 * t[...,1:])
+                                             )
+        ),
+    ])
+    return model
+
+def build_diabetes_model(train_ds, val_ds,  feature_layer,  epochs=5, loss_metric='mse'):
+    model = build_sequential_model(feature_layer)
+    model.compile(optimizer=optimizer, loss=loss_metric, metrics=[loss_metric])
+    early_stop = tf.keras.callbacks.EarlyStopping(monitor=loss_metric, patience=3)     
+    history = model.fit(train_ds, validation_data=val_ds,
+                        callbacks=[early_stop],
+                        epochs=epochs)
+    return model, history
+  
+diabetes_model, history = build_diabetes_model(diabetes_train_ds, diabetes_val_ds,  claim_feature_layer,  epochs=20)
+
+
+# Show Model Uncertainty Range with TF Probability
+feature_list = student_categorical_col_list + student_numerical_col_list
+diabetes_x_tst = dict(d_test[feature_list])
+diabetes_yhat = diabetes_model(diabetes_x_tst)
+preds = diabetes_model.predict(diabetes_test_ds)
+
+from student_utils import get_mean_std_from_preds
+m, s = get_mean_std_from_preds(diabetes_yhat)
+
+# Show Prediction Output
+prob_outputs = {
+    "pred": preds.flatten(),
+    "actual_value": d_test['time_in_hospital'].values,
+    "pred_mean": m.numpy().flatten(),
+    "pred_std": s.numpy().flatten()
+}
+prob_output_df = pd.DataFrame(prob_outputs)
+
+# Convert Regression Output to Classification Output for Patient Selection
+from student_utils import get_student_binary_prediction
+student_binary_prediction = get_student_binary_prediction(prob_output_df, 'pred_mean')
+
+def add_pred_to_test(test_df, pred_np, demo_col_list):
+    for c in demo_col_list:
+        test_df[c] = test_df[c].astype(str)
+    test_df['score'] = pred_np
+    test_df['label_value'] = test_df['time_in_hospital'].apply(lambda x: 1 if x >=5 else 0)
+    return test_df
+pred_test_df = add_pred_to_test(d_test, student_binary_prediction, ['race', 'gender'])
+pred_test_df[['patient_nbr', 'gender', 'race', 'time_in_hospital', 'score', 'label_value']].head()
+
+# Model Evaluation Metrics
+from sklearn.metrics import accuracy_score, f1_score, classification_report, roc_auc_score
+y_true = pred_test_df['label_value'].values 
+y_pred = pred_test_df['score'].values
+print(classification_report(y_true, y_pred))
+roc_auc_score(y_true, y_pred)
+
+## Evaluating Potential Model Biases with Aequitas Toolkit
+# Prepare Data For Aequitas Bias Toolkit
+# Aequitas
+from aequitas.preprocessing import preprocess_input_df
+from aequitas.group import Group
+from aequitas.plotting import Plot
+from aequitas.bias import Bias
+from aequitas.fairness import Fairness
+
+ae_subset_df = pred_test_df[['race', 'gender', 'score', 'label_value']]
+ae_df, _ = preprocess_input_df(ae_subset_df)
+g = Group()
+xtab, _ = g.get_crosstabs(ae_df)
+absolute_metrics = g.list_absolute_metrics(xtab)
+clean_xtab = xtab.fillna(-1)
+aqp = Plot()
+b = Bias()
+
+# Reference Group Selection
+# test reference group with Caucasian Male
+bdf = b.get_disparity_predefined_groups(clean_xtab, 
+                    original_df=ae_df, 
+                    ref_groups_dict={'race':'Caucasian', 'gender':'Male'
+                                     }, 
+                    alpha=0.05, 
+                    check_significance=False)
+
+
+f = Fairness()
+fdf = f.get_group_value_fairness(bdf)
+
+# Race and Gender Bias Analysis for Patient Selection
+# Plot two metrics
+
+# Is there significant bias in your model for either race or gender?
+p = aqp.plot_group_metric_all(xtab, metrics=['tpr', 'ppr'], ncols=2)
+
+# Fairness Analysis Example - Relative to a Reference Group
+# Reference group fairness plot
+fpr_disparity = aqp.plot_disparity(bdf, group_metric='fpr_disparity', 
+                                       attribute_name='race')
